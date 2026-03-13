abc31486addba3ad693811da34e4164e5c5c3e73
GabrielFachin <gabrielfachinoliveira@gmail.com>
2026-03-13 12:37:20 -0300

    [BACKEND] feat: first commit, sheet integrada

---
+++ BackEnd/sheets.py
@@ -0,0 +1,20 @@
+import gspread
+from pathlib import Path
+from google.oauth2.service_account import Credentials
+
+DIRETORIO_ATUAL = Path(__file__).resolve().parent
+
+
+AUTH_PATH = DIRETORIO_ATUAL / 'Data' / 'elated-coil-490113-n2-0a97f7027a26.json'
+
+scopes = ['https://www.googleapis.com/auth/drive']
+
+creds = Credentials.from_service_account_file(str(AUTH_PATH), scopes = scopes)
+
+client = gspread.authorize(creds)
+
+sheetID = "1m-QxfHDEthX1-wS7l15ilTp2Ah1m2Sn4UyOXp4AI3u8"
+
+sheet = client.open_by_key(sheetID)
+
+print(sheet.sheet1.row_values(1))
